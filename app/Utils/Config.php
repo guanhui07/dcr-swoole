0d9280fcf8893d952ab19d1159c8fba873d0e088
guanhui07 <guanhui07@gmail.com>
2024-10-25 19:52:49 +0800

    fixed

--- app/Utils/Config.php
+++ app/Utils/Config.php
@@ -1,11 +1,112 @@
 <?php
+declare(strict_types = 1);
+/**
+ * The file is part of Dcr/framework
+ *
+ *
+ */
 
 namespace App\Utils;
 
+use Illuminate\Support\Arr;
+
 class Config
 {
-    public static function get()
+    public static function get($str = 'app')
     {
+        // config('app.debug');
+        if (str_contains($str, '.')) {
+            $tmpArr = explode('.', $str, 2);
 
+            $arr = require(PROJECT_ROOT.'config/'.$tmpArr[0].'.php');
+            $end = end($tmpArr);
+            $arr = Arr::get($arr, $end, '');
+            return self::dealArr($arr);
+        }
+
+        // config('app')
+        $arr = require(PROJECT_ROOT.'config/'.$str.'.php');
+        $arr = self::dealArr($arr);
+
+        return $arr;
+    }
+
+    /**
+     * @param  mixed  $arr
+     *
+     * @return bool|mixed
+     */
+    protected static function dealArr(mixed $arr): mixed
+    {
+        if (!is_array($arr)) {
+            switch (strtolower($arr)) {
+                case 'true':
+                case '(true)':
+                    return true;
+                case 'false':
+                case '(false)':
+                    return false;
+                case 'empty':
+                case '(empty)':
+                    return '';
+                case 'null':
+                case '(null)':
+                    return null;
+                default:
+                    return $arr;
+            }
+        }
+        $arr = self::processConfigArr($arr);
+        return $arr;
+    }
+
+    /**
+     * @param  mixed  $arr
+     *
+     * @return bool
+     */
+    protected static function isTrue(mixed $arr): bool
+    {
+        return is_string($arr) && $arr === 'true';
+    }
+
+    /**
+     * @param  mixed  $arr
+     *
+     * @return bool
+     */
+    protected static function isFalse(mixed $arr): bool
+    {
+        return is_string($arr) && $arr === 'false';
+    }
+
+    /**
+     * @param  array  $arr
+     *
+     * @return bool|mixed
+     */
+    protected static function processConfigArr(array $arr)
+    {
+        return array_map(function ($value) {
+            if (!is_string($value)) {
+                return $value;
+            }
+            switch (strtolower($value)) {
+                case 'true':
+                case '(true)':
+                    return true;
+                case 'false':
+                case '(false)':
+                    return false;
+                case 'empty':
+                case '(empty)':
+                    return '';
+                case 'null':
+                case '(null)':
+                    return null;
+                default:
+                    return $value;
+            }
+        }, $arr);
     }
 }
--- app/Utils/Log.php
+++ app/Utils/Log.php
@@ -1,16 +1,56 @@
 <?php
+declare(strict_types=1);
+/**
+ * The file is part of Dcr/framework
+ *
+ *
+ */
 
 namespace App\Utils;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 class Log
 {
-    public static function debug(string $message)
+    public static function info($str): void
     {
-        echo $message.PHP_EOL;
+        $log = self::getLogger(Logger::INFO);
+        $log->warning((string)$str);
     }
 
-    public static function error(string $message)
+    public static function debug($str): void
     {
-        echo $message.PHP_EOL;
+        $log = self::getLogger(Logger::INFO);
+        $log->warning((string)$str);
+    }
+
+    public static function warning($str): void
+    {
+        $log = self::getLogger(Logger::WARNING);
+        $log->warning((string)$str);
+    }
+
+    public static function error($str): void
+    {
+        $log = self::getLogger(Logger::ERROR);
+        // add records to the log
+        $log->error((string)$str);
+    }
+
+    public static function write($str, $config = ''): void
+    {
+        self::error($str);
+    }
+
+    /**
+     * @param int $level
+     * @return Logger
+     */
+    public static function getLogger($level = Logger::WARNING): Logger
+    {
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler(PROJECT_ROOT . 'runtime/log.log', $level));
+        return $log;
     }
 }
